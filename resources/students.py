b7af11788662d8f14562d6a9da9e15e8e63025a5
Zihao C <62597814+czh2152215@users.noreply.github.com>
2023-09-11 17:59:04 -0400

    task1

--- resources/students.py
+++ resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/Users/donaldferguson/Dropbox/000-NewProjects/E6156F23/first_microservice/resources/students.json"
+        "C:/Users/Lenovo/Documents\GitHub/-e6156-microservice-1/resources/students.json"
 
     def __init__(self):
         self.students = None
